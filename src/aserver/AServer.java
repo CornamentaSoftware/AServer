101424a2b11ce517fe19743b527520dca1d2ccc6
AnaSchz <mckagan99@gmail.com>
2017-11-29 00:40:29 -0600

    Ya

--- src/aserver/AServer.java
+++ src/aserver/AServer.java
@@ -21,41 +21,43 @@ public class AServer {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws UnknownHostException, SocketException, IOException, Exception {
-        int puertoTS= 3000;
-        int miPuerto= 5000;
-        InetAddress ipTS= InetAddress.getByName("192.168.9.255");
-        //IP del servidor de tickets, pero quién sabe en qué máquina quede
-        Socket socket = new Socket(miPuerto);
+        while(true){
+            int puertoTS= 3000;
+            int miPuerto= 5000;
+            InetAddress ipTS= InetAddress.getByName("192.168.9.255");
+            //IP del servidor de tickets, pero quién sabe en qué máquina quede
+            Socket socket = new Socket(miPuerto);
 
-        String mensaje = socket.recibe();
-        String datos[] = new String[3];
-        
-        //Aquí checha qué datos recibió y los mete en un arreglo
-        for(int i=0, j=0; i<mensaje.length() && j<3; i++){
-            if(mensaje.charAt(i)==' '){
-                datos[j] = mensaje.substring(0,i);
-                mensaje = mensaje.substring(i+1);
-                i=0;
-                j++;
+            String mensaje = socket.recibe();
+            String datos[] = new String[3];
+
+            //Aquí checa qué datos recibió y los mete en un arreglo
+            for(int i=0, j=0; i<mensaje.length() && j<3; i++){
+                if(mensaje.charAt(i)==' '){
+                    datos[j] = mensaje.substring(0,i);
+                    mensaje = mensaje.substring(i+1);
+                    i=0;
+                    j++;
+                }
             }
+
+            //Busca al usuario
+            Base base = new Base();
+            if(base.buscarUsuario(datos[2])){
+                mensaje = datos[0] + " " + datos[1];
+
+                AES cifrar = new AES();
+                String ticketI = cifrar.Encriptar(mensaje, "CFRR");
+                socket.envia(ipTS, puertoTS, ticketI);
+            }
+            //Si existe, se manda su IP y el servicio que requiere al servidor de tickets, 
+            //cifrado con AES y con la clave CFRR, que ambos conocen
+            else{
+                socket.envia(InetAddress.getByName(datos[0]), 4000, "Error");
+            }
+            //Si no existe, se envía un mensaje de error al cliente xdxd
+            socket.close();
         }
-        
-        //Busca al usuario
-        Base base = new Base();
-        if(base.buscarUsuario(datos[2])){
-            mensaje = datos[0] + " " + datos[1];
-            
-            AES cifrar = new AES();
-            String ticketI = cifrar.Encriptar(mensaje, "CFRR");
-            socket.envia(ipTS, puertoTS, ticketI);
-        }
-        //Si existe, se manda su IP y el servicio que requiere al servidor de tickets, 
-        //cifrado con AES y con la clave CFRR, que ambos conocen
-        else{
-            socket.envia(InetAddress.getByName(datos[0]), 4000, "Error");
-        }
-        //Si no existe, se envía un mensaje de error al cliente xdxd
-        
     }
     
 }
